97e2a5694658ba7bd5d191a7e3fc5b542595df95
abdessittir harkaty <abdessattarharkaty@gmail.com>
2023-02-05 15:45:44 +0100

    Prevent multiple connections to database

--- server.js
+++ server.js
@@ -12,11 +12,17 @@ const io = new Server(httpServer, {
   }
 });
 
+let isConnected = false;
 async function connectDB() {
   const client = new MongoClient(process.env.CONNECTION_URL);
+  if (isConnected) {
+    console.log("database already connected");
+    return client.db("chat");
+  }
 
   try {
     await client.connect();
+    isConnected = true;
     console.log("connected to database successfully");
   } catch (err) {
     console.error(err);
