2ee158ff8ddb21c1ad43c356878f19c1739db2eb
abdessittir harkaty <abdessattarharkaty@gmail.com>
2023-02-05 11:55:26 +0100

    Integrate db and send message functionality

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 import { Server } from "socket.io";
 import { createServer } from "http";
+import { MongoClient, ObjectId } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const httpServer = createServer();
 const io = new Server(httpServer, {
@@ -8,16 +12,48 @@ const io = new Server(httpServer, {
   }
 });
 
+async function connectDB() {
+  const client = new MongoClient(process.env.CONNECTION_URL);
+
+  try {
+    await client.connect();
+    console.log("connected to database successfully");
+  } catch (err) {
+    console.error(err);
+  }
+
+  return client.db("chat");
+}
+
 io.on('connection', (socket) => {
   socket.on('user', (id) => {
-    console.log(id);
     socket.join(id);
   });
 
   socket.on('typing', ({chatId, users}) =>{
     users.forEach(id => {
       socket.to(id).emit('typing-received', chatId);
-    })
+    });
+  });
+
+  socket.on('typing-ended', ({chatId, users}) => {
+    users.forEach(id => {
+      socket.to(id).emit('typing-ended-reveived', chatId);
+    });
+  });
+
+  socket.on('message', async ({chatId, users, message}) => {
+    try {
+      const DB = await connectDB();
+      const Chat = DB.collection('chats');
+      const chat = await Chat.updateOne({_id: new ObjectId(chatId)}, { $push: { messages: message } }).then(() => {
+        users.forEach(id => {
+          socket.to(id).emit('message-received', {chatId, message});
+        });
+      })
+    } catch (err) {
+      console.log(err);
+    }
   });
 });
 
